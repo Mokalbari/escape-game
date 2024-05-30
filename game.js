027915316aa16bfcd790bee3cbc1c11e14ba4b0d
sylvain33220 <sylvain.poteaux@gmail.com>
2024-05-30 16:53:08 +0200

    creat modalUser and css for modal and create from modal  to  html

--- game.js
+++ game.js
@@ -9,6 +9,74 @@ const user = {
   hammer: false,
 };
 
+const modalUser = () => 
+{
+  const modal = document.getElementById("userModal");
+  const btn = document.getElementById("openModalBtn");
+  const span = document.getElementsByClassName("close")[0];
+  const saveBtn = document.getElementById("savePseudoBtn");
+  const input = document.getElementById("userPseudoInput");
+
+  let pseudo = "";
+
+  btn.onclick = function () {modal.style.display = "block";}
+  span.onclick = function () {modal.style.display = "none";}
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
+    saveBtn.onclick = function() 
+    {       
+        pseudo = input.value;
+        console.log("Pseudo enregistré:", pseudo);
+    
+        if(!input.value) {
+                alert("tu n'as pas entré de pseudo")
+        }else if (input.value === "poulet" || input.value ==="Poulet") {
+            alert ("on t'as reconnue abdou ! ")
+            modal.style.display ="none";
+        }else if (input.value === "abdou" || input.value ==="Abdou") {
+            alert(`hey c'est toi mon poulet ?`)
+            modal.style.display = "none"
+        }
+        else {
+            alert(`salut ${pseudo} tu es près a entrer dans le jeu !` )
+            modal.style.display = "none";     
+            }
+    
+    }
+  
+  submitBtn.KeyboardEventInit = (event)=> {
+    const submitBtn = document.addEventListener("submit");
+    input.addEventListener("keypress", (event) =>{
+             if (event.KeyChar === 13 ) {
+                 pseudo = input.value;
+             }
+         })
+ 
+  }
+}
+    modalUser();
+// let UserPseudo = "";
+// const modalUser = {
+//   modal : document.getElementById("userModal"),
+//   btn :   document.getElementById("openModalBtn"),
+//   span :  document.getElementById("close")[0],
+//   saveBtn : document.getElementById("savePseudoBtn"),
+//   input : document.getElementById("userPseudoInput"),
+// };
+//  const userClick = () => {
+//           btn.onclick = modal.style.display = "block",
+//           span.onclick = modal.style.display = "none"        
+//  };
+//       saveBtn.onclick = () => {
+//           pseudo = input.value;
+//           modal.style.display = "none"
+//  };
+
+
 /* l'objet gameItem doit faire évoluer ses conditions lorsque l'user peut intéragir 
 avec un élément statique.*/
 const gameItem = {
